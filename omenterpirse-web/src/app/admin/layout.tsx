0e357673aa9b41673f68089cab25b4541afe7009
Vedabhavishya <vedabhavishya.gudivaka.com>
2026-07-03 11:01:51 +0530

    Remove Home Tabs section from admin panel

--- omenterpirse-web/src/app/admin/layout.tsx
+++ omenterpirse-web/src/app/admin/layout.tsx
@@ -30,7 +30,6 @@ const sidebarLinks = [
   { name: "Orders", href: "/admin/orders", icon: Package },
   { name: "Packaging", href: "/admin/settings/packaging", icon: Box },
   { name: "Home Banners", href: "/admin/home-banners", icon: Sparkles },
-  { name: "Home Tabs", href: "/admin/home-tabs", icon: LayoutDashboard },
   { name: "Customers", href: "/admin/customers", icon: Users },
 ];
 
